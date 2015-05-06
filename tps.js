4979cc0011c62bcc8c87c8aaa1773a106a732c27
Benjamin Pang <molarmanful@users.noreply.github.com>
2015-05-06 16:15:36 -0700

    Update tps.js

--- tps.js
+++ tps.js
@@ -80,13 +80,13 @@ slicedepth = 0;
 		});
 	};
 })( jQuery );
-$.getScript(['https://rawgit.com/cubing/jsss/master/scramble_222.js', 'https://rawgit.com/cubing/jsss/master/scramble_333.js', 'https://rawgit.com/cubing/jsss/master/scramble_NNN.js', 'https://rawgit.com/cubing/jsss/master/scramble_minx.js', 'https://rawgit.com/cubing/jsss/master/scramble_pyram.js', 'https://rawgit.com/cubing/jsss/master/scramble_sq1.js', 'https://rawgit.com/cubing/jsss/master/scramble_clock.js', 'https://molarmanful.github.io/minimalistimer/skewb.js'], function(){
-  $.each(event, function(i, v){
-    scramblers[v].initialize(null, Math);
-  });
-});
 
 window.tps = function(s, t){
+  $.getScript(['https://rawgit.com/cubing/jsss/master/scramble_222.js', 'https://rawgit.com/cubing/jsss/master/scramble_333.js', 'https://rawgit.com/cubing/jsss/master/scramble_NNN.js', 'https://rawgit.com/cubing/jsss/master/scramble_minx.js', 'https://rawgit.com/cubing/jsss/master/scramble_pyram.js', 'https://rawgit.com/cubing/jsss/master/scramble_sq1.js', 'https://rawgit.com/cubing/jsss/master/scramble_clock.js', 'https://molarmanful.github.io/minimalistimer/skewb.js'], function(){
+    $.each(event, function(i, v){
+      scramblers[v].initialize(null, Math);
+    });
+  });
   timer_obj = new startTimer($(t));
   me = this;
   this.scramble = function(){
